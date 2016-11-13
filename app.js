6cfec068ca042ef83be589c4e921f2638f16d5db
esotericabsurdist <esotericabsurdist@gmail.com>
2016-11-12 23:07:05 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+
+// app
+var app = express();
+
+
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//socket io server
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+server.listen(4200);//server.listen(process.env.PORT || 4200);
+// listen for connection
+io.sockets.on('connection', function(client) {
+
+    console.log('Client connected...');
+
+    //if client says 'join', get their data and print it to the console.
+    client.on('join', function(data) {
+        console.log(data);
+    });
+});
+
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
